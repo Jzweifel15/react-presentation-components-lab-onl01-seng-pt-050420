c883616d83bb7d36a3dd8b47eb4bb9c6a0b5a617
Justin R. Zweifel <jzweifel15@yahoo.com>
2021-01-25 23:49:27 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import SimplerComponent from './components/SimplerComponent';
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={"hungry"} />
   </div>,
   document.getElementById('root')
 );
